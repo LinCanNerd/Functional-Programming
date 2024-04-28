deade18bc8063919ed3557fc6c8165f689ccc5db
LinCan <LincanECDL@gmail.com>
2024-04-28 20:43:06 +0200

    add hw3

--- alberi/hw2-Lin-Can-1994375.hs
+++ alberi/hw2-Lin-Can-1994375.hs
@@ -1,7 +1,4 @@
-{-# LANGUAGE AllowAmbiguousTypes #-}
 import Debug.Trace
-import Text.XHtml (height)
-
 -- Homework 2, Lin Can  ID 1994375
 
 --Ex1 definire il mergeSort iterativo
@@ -90,12 +87,13 @@ foldlBT2 f z (Node2 l r) = foldlBT2 f (foldlBT2 f z l) r
 
 
 --2.2
+
+--non sono riuscito a fare le funzioni per BT2
+
 --numero dei nodi di un albero binario
 countNodesBT1 :: BinTree1 a -> Int
 countNodesBT1 = foldrBT1 (\_ acc -> 1 + acc) 0
 
-countNodesBT2 :: BinTree2 a -> Int
-countNodesBT2 = foldrBT2 (\_ acc -> 1 + acc) 0
 
 --altezza dell'albero binario
 foldrHBT1 :: (a -> b -> b -> b) -> b -> BinTree1 a -> b
@@ -105,10 +103,6 @@ foldrHBT1 f z (Node1 x l r) = f x (foldrHBT1 f z r) (foldrHBT1 f z l)
 
 heightBT1 :: BinTree1 a -> Int
 heightBT1 = foldrHBT1 (\_ l r -> 1 + max l r) 0
-
-
-
-
 
 -- massimo sbilanciamento tra i sottoalberi sinistro e destro
 max_sbilanciamentoBT1 :: BinTree1 a -> Int
@@ -131,6 +125,9 @@ foldlT f z (R x ts) = foldl (foldlT f) (f z x) ts
 
 
 --3 Nodi Equilibrati
+--La complessità della funzione è in O(n), perchè attraversa ogni nodo due volte,
+--una volta per andare fino in fondo e calcolare la somma dal root a ogni nodo,e una volta per salire
+-- e confrontare la somma dei subtree con la somma del root
 
 nodiEquilibrati :: (Num a, Eq a) => BinTree1 a -> [a]
 nodiEquilibrati tree = snd $ aux tree 0
@@ -142,9 +139,9 @@ nodiEquilibrati tree = snd $ aux tree 0
       where
         (leftSum, balancedLeft) = aux left (sumFromRoot + x)
         (rightSum, balancedRight) = aux right (sumFromRoot + x)
-        currentSum = x + leftSum + rightSum
+        currentSum = leftSum + rightSum + x
         -- Controlla se il nodo corrente è equilibrato
-        currentBalanced = [x | sumFromRoot == leftSum + rightSum]
+        currentBalanced = [x | sumFromRoot == currentSum] 
         result = currentBalanced ++ balancedLeft ++ balancedRight
 
 
@@ -153,14 +150,16 @@ nodiEquilibrati tree = snd $ aux tree 0
 
 
 --4 Alberi Binari di Ricerca
+-- la complessità di listToABR è di O(nlogn) perchè il sort richiede O(nlogn)
+-- e la costruzione dell'albero richiede O(n), se la lista è già ordinata la complessità è O(n)
 listToABR :: Ord a => [a] -> BinTree1 a
-listToABR = foldr insert Empty
-  where
-    insert :: Ord a => a -> BinTree1 a -> BinTree1 a
-    insert x Empty = Node1 x Empty Empty
-    insert x (Node1 y l r)
-      | x < y     = Node1 y (insert x l) r
-      | otherwise = Node1 y l (insert x r)
+listToABR = aux . iterativeMergeSort2 
+ where aux :: Ord a => [a] -> BinTree1 a
+       aux [] = Empty
+       aux xs = Node1 (xs !! mid) (aux left) (aux right)
+         where mid = length xs `div` 2
+               left = take mid xs
+               right = drop (mid + 1) xs
 
 
 
@@ -180,35 +179,40 @@ myScanr f e xs = myReverse (myScanl f  e (myReverse xs))where
   myScanl f e (x:xs) = e : myScanl f (f x e) xs
 
 
+
+
+--TEST--
 instance (Show a) => Show (BinTree1 a) where
     show :: Show a => BinTree1 a -> String
     show Empty = "Empty"
-    show (Node1 x l r) = "Node1 (" ++ show x ++ ")\n (" ++ show l ++ ")\n (" ++ show r ++ ")\n"
+    show (Node1 x l r) = "Node1 (" ++ show x ++ ") (" ++ show l ++ ") (" ++ show r ++ ")"
 
 instance (Show a) => Show (BinTree2 a) where
     show :: Show a => BinTree2 a -> String
     show (Leaf x) = "Leaf (" ++ show x ++ ")"
     show (Node2 l r) = "Node2 (" ++ show l ++ ") (" ++ show r ++ ")"
 
-
+instance (Show a ) => Show (Tree a) where
+    show :: Show a => Tree a -> String
+    show (R x ts) = "R " ++ show x ++ "  [" ++ show ts ++ "]"
 
 main = do
   let myTree1 :: BinTree1 Int
       myTree1 = Node1 1
                   (Node1 2
+                      (Node1 1 (Node1 2 Empty Empty) Empty)
                       (Node1 3 Empty Empty)
-                      (Node1 4 Empty Empty)
                   )
                   (Node1 5
                       (Node1 6 Empty Empty)
-                      (Node1 7 Empty Empty)
+                      (Node1 6 Empty Empty)
                   )
 
 
   let myTree2 :: BinTree2 Int
       myTree2 = Node2
                   (Node2
-                      (Leaf 1)
+                      (Leaf 0)
                       (Leaf 2)
                   )
                   (Node2
@@ -220,7 +224,7 @@ main = do
       myBtree = Node1 1
                   (Node1 2
                       (Node1 3 Empty Empty)
-                      (Node1 4 Empty Empty)
+                      (Node1 0 Empty Empty)
                   )
                   (Node1 5
                       (Node1 6 Empty Empty)
@@ -228,9 +232,8 @@ main = do
                   )
 
   let myT :: Tree Int
-      myT = R 4 [ R 2 [R 3 [], R 4 []]
-                             ]
+      myT = R 4 [ R 2 [R 3 [], R 4 [], R 5 []]]
 
-  let mylist = [1,2,3]
-  let treeSum = nodiEquilibrati myTree1
-  print treeSum
+  let mylist = [5,3,4,2,1,8,0]
+  let result = foldrT (-) 0 myT 
+  print result
---
+++ liste infinite/hw3.hs
@@ -0,0 +1,28 @@
+import Distribution.Parsec (Parsec(parsec))
+--hw3 Can Lin 
+
+--ex1 Insomnia
+
+insonnia :: String
+insonnia = concatMap (\n -> show n ++ " sheep ") [1..]
+
+
+--ex2 Triangolo di Tartaglia
+tartaglia :: [[Int]]
+tartaglia = iterate nextRow [1]
+  where
+    nextRow row = zipWith (+) ([0] ++ row) (row ++ [0])
+
+
+--ex3 Numeri fortunati
+numeriFortunati :: [Int]
+numeriFortunati = 1 : sieve [3,5..] 3
+    where
+    sieve (x:xs) n = x : sieve [y | (i,y) <- zip[n..] xs, i `mod` x /= 0] (n+1)
+       
+
+
+main :: IO ()
+--main = putStrLn $ take 5 insonnia
+--main = print $ take 10 tartaglia
+main = print $ take 10 numeriFortunati
