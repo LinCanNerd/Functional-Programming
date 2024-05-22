1bd416417ede79f0907eac56f63af28e2416516a
LincanNerd <LincanECDL@gmail.com>
2024-05-22 16:34:36 +0200

    update es1

--- monadi/hw4.hs
+++ monadi/hw4.hs
@@ -1,27 +1,40 @@
+import Control.Monad (replicateM)
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 import Data.Map (Map)
 import Control.Monad.State
 import Control.Applicative ()
+
+
 --Matricola: 1994375 Can Lin
 
 --ex1 Input/Output
+
 charCount :: IO ()
 charCount = do
-    putStrLn "Enter n:"
+    -- Prende n come input
+    putStrLn "Enter the number of strings:"
     n <- readLn
-    putStrLn "Enter the string:"
-    strings <- getLine
-    let new_string = take n strings
-    let count = countChar new_string
-    putStrLn "Character frequencies:"
-    forM_ (Map.toList count) $ \(c, freq) ->
-        putStrLn $ c : ": " ++ show freq
-
-countChar :: String -> Map Char Int
-countChar = foldl updateMap Map.empty
+    -- legge n stringhe
+    putStrLn "Enter the strings:"
+    strings <- replicateM n getLine
+    -- fa il conteggio dei caratteri
+    let charOccurrences = countCharsInStrings strings
+    -- printa il conteggio
+    mapM_ (printCount charOccurrences) ['a'..'z']
   where
-    updateMap :: Map Char Int -> Char -> Map Char Int
-    updateMap m c = Map.insertWith (+) c 1 m
+    --funzione ausiliaria print
+    printCount :: Map Char Int -> Char -> IO ()
+    printCount occurrences char = putStrLn $ char : ": " ++ show (Map.findWithDefault 0 char occurrences)
+
+-- Funzione per contare le occorrenze dei caratteri nelle stringhe, RICEVE SOLO CARATTERI MINUSCOLI
+countCharsInStrings :: [String] -> Map Char Int
+countCharsInStrings strings =
+    let charSets = map Set.fromList strings
+        initialMap = Map.fromList [(char, 0) | char <- ['a'..'z']]
+    in foldr (Map.adjust (+1)) initialMap (concatMap Set.toList charSets)
+
+
 
 
 --ex2 Nodi Equilibrati con applicativi e monadi
@@ -61,7 +74,7 @@ nodiEquilibrati tree = evalState (aux tree 0) (getRootValue tree)
     subtreeSum :: Num a => BinTree a -> a
     subtreeSum Empty = 0
     subtreeSum (Node x left right) = x + subtreeSum left + subtreeSum right
-      
+
 
 --ex3 Monadi/Eccezioni
 
@@ -111,11 +124,6 @@ modNatBin a b = Just (intToNatBin $ natBinToInt a `mod` natBinToInt b)
 
 
 
-
-
-
-
-
 -- Example binary tree
 exampleTree :: BinTree Int
 exampleTree = Node 10 (Node 10 (Node 20 Empty Empty) Empty) (Node 10 Empty Empty)
@@ -123,5 +131,5 @@ exampleTree = Node 10 (Node 10 (Node 20 Empty Empty) Empty) (Node 10 Empty Empty
 -- Main function to run the test
 main :: IO ()
 main = do
-  let result = intToNatBin 10
+  let result = nodiEquilibrati exampleTree
   print result
