0cd7832637d6f8e8c31ee4120e514af640819610
LincanNerd <LincanECDL@gmail.com>
2024-06-05 12:03:52 +0200

    Update hw3-LinCan-1994375.hs

--- liste infinite/hw3-LinCan-1994375.hs
+++ liste infinite/hw3-LinCan-1994375.hs
@@ -18,18 +18,6 @@ numeriFortunati :: [Int]
 numeriFortunati = 1 : sieve [3,5..] 3
     where
     sieve (x:xs) n = x : sieve [y | (i,y) <- zip[n..] xs, i `mod` x /= 0] (n+1)
-       
-
------OPTIONAL-----
-
---1D Ricorsione primitiva--  
-primRec :: (Int -> a -> a) -> a -> Int -> a
-primRec f g 0 = g
-primRec f g n = f (n-1) (primRec f g (n-1)) 
-
-primRec2 :: (Int -> a -> a) -> a -> Int -> a
-primRec2 f g n = snd $ foldl (\(i, acc) _ -> (i + 1, f i acc)) (0, g) [1..n]
-
 
 
 
